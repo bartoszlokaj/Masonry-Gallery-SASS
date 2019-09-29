39bab31176dbcb87f189c15d556bef51926b71ad
Bartosz <bartoszlokaj@gmail.com>
2019-09-29 21:53:18 +0200

    modal animation tweeked

--- src/js/Modal.js
+++ src/js/Modal.js
@@ -1,24 +1,24 @@
 const modal = () => {
   const tiles = document.querySelectorAll(".tile"),
-        backdrop = document.querySelector(".backdrop"),
-        modal = document.querySelector(".modal"),
-        close = document.querySelector(".close");
+    backdrop = document.querySelector(".backdrop"),
+    modal = document.querySelector(".modal"),
+    close = document.querySelector(".close");
 
   tiles.forEach(el => {
     el.addEventListener("click", () => {
-      backdrop.classList.add("backdropActive");
-      modal.classList.add("modalActive");
+      backdrop.style.animation = "backdropIn .2s ease-in-out forwards";
+      modal.style.animation = "modalIn .2s ease-in-out forwards";
     });
   });
 
   backdrop.addEventListener("click", () => {
-    backdrop.classList.toggle("backdropActive");
-    modal.classList.toggle("modalActive");
+    backdrop.style.animation = "backdropOut .2s ease-in-out forwards";
+    modal.style.animation = "modalOut .2s ease-in-out forwards";
   });
 
   close.addEventListener("click", () => {
-    backdrop.classList.toggle("backdropActive");
-    modal.classList.toggle("modalActive");
+    backdrop.style.animation = "backdropOut .2s ease-in-out forwards";
+    modal.style.animation = "modalOut .2s ease-in-out forwards";
   });
 };
 
